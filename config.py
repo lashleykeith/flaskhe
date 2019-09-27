bad6e57dacb28913cb7ee68072ccb81eb12554fe
Keith Lashley <lashleykeith@gmail.com>
2019-09-27 16:07:22 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'applesandoranges'
+DEBUG = True
+
+MONGODB_DB = 'partygo'
+MONGODB_HOST = 'mongodb://127.0.0.1:27017'
+MONGODB_PORT = 27017
+MONGODB_USERNAME = 'partygoAdmin'
+MONGODB_PASSWORD = 'flashlantern'
